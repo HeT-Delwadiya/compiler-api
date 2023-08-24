6ffdde7251adf0dedd883facd251fbdd67ec8717
HeT-Delwadiya <hetdelwadiya@gmail.com>
2023-08-24 21:30:31 +0530

    fixed module export issue

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 const fetch = require('node-fetch');
 
 const fetchResult = async (id) => {
-       const options = {
-              'method': 'GET',
-              'headers': {
-                     'Accept': '*/*',
-              }
-       }
+	const options = {
+		'method': 'GET',
+		'headers': {
+			'Accept': '*/*',
+		}
+	}
 
-       try {
-              const fetchResultAPIRes = await fetch(`https://codejudge.geeksforgeeks.org/get-status/${id}`, options);
-              const fetchResultAPIOutput = await fetchResultAPIRes.json();
+	try {
+		const fetchResultAPIRes = await fetch(`https://codejudge.geeksforgeeks.org/get-status/${id}`, options);
+		const fetchResultAPIOutput = await fetchResultAPIRes.json();
 
-              if (fetchResultAPIOutput.status=="SUCCESS") {
-                     return fetchResultAPIOutput
-              } else if (fetchResultAPIOutput.status=="in-queue") {
-                     await fetchResult(id)
-              } 
-       } catch (error) {
-              throw new Error(error);
-       } 
+		if (fetchResultAPIOutput.status == "SUCCESS") {
+			return fetchResultAPIOutput
+		} else if (fetchResultAPIOutput.status == "in-queue") {
+			return await fetchResult(id)
+		}
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
-exports.compilerApi = async (data, callback) => {
+const compilerApi = async (data) => {
+	const options = {
+		'method': 'POST',
+		'headers': {
+			'Accept': '*/*',
+			'Content-Type': 'application/json; charset=utf-8',
+		},
+		'body': JSON.stringify({
+			"language": data.language,
+			"code": data.code,
+			"input": data.input,
+			"save": false
+		})
+	}
 
-       const options = {
-              'method': 'POST',
-              'headers': {
-                     'Accept': '*/*',
-                     'Content-Type': 'application/json; charset=utf-8',
-              },
-              'body': JSON.stringify({
-                     "language": data.language,
-                     "code": data.code,
-                     "input": data.input,
-                     "save": false
-              })
-       }
-
-       try {
-              const compileCodeAPIRes = await fetch('https://codejudge.geeksforgeeks.org/submit-request', options);
-              const compileCodeAPIOutput = await compileCodeAPIRes.json();
-              callback(await fetchResult(compileCodeAPIOutput.id));
-       } catch (error) {
-              throw new Error(error);
-       }
+	try {
+		const compileCodeAPIRes = await fetch('https://codejudge.geeksforgeeks.org/submit-request', options);
+		const compileCodeAPIOutput = await compileCodeAPIRes.json();
+		const data = await fetchResult(compileCodeAPIOutput.id);
+		return data;
+	} catch (error) {
+		throw new Error(error);
+	}
 }
+
+module.exports.compilerApi = compilerApi;
